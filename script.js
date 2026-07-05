e36f3b077f7817832a167253adff292bb21a32db
om-bhagat <139191838+om-bhagat@users.noreply.github.com>
2026-07-05 11:33:44 -0400

    Add complete budget form inputs

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 const budgetForm = document.getElementById("budgetForm");
 
+const destinationInput = document.getElementById("destination");
+const daysInput = document.getElementById("days");
+const travelersInput = document.getElementById("travelers");
+const hotelCostInput = document.getElementById("hotelCost");
+const foodCostInput = document.getElementById("foodCost");
+const transportCostInput = document.getElementById("transportCost");
+const activityCostInput = document.getElementById("activityCost");
+const extraPercentInput = document.getElementById("extraPercent");
+
+const errorMessage = document.getElementById("errorMessage");
+
 budgetForm.addEventListener("submit", function(event) {
     event.preventDefault();
 
-    console.log("budget form submitted,");
+    const destination = destinationInput.value;
+    const days = daysInput.value;
+    const travelers = travelersInput.value;
+    const hotelCost = hotelCostInput.value;
+    const foodCost = foodCostInput.value;
+    const transportCost = transportCostInput.value;
+    const activityCost = activityCostInput.value;
+    const extraPercent = extraPercentInput.value;
+
+    console.log("destination", destination);
+    console.log("Days:", days);
+    console.log("Travelers:", travelers);
+    console.log("Hotel Cost:", hotelCost);
+    console.log("Food Cost:", foodCost);
+    console.log("Transportation Cost:", transportCost);
+    console.log("Activity Cost:", activityCost);
+    console.log("Extra Percent:", extraPercent);
+
+    errorMessage.textContent = "Form values are connected. Calculations ill be added next."
 });
